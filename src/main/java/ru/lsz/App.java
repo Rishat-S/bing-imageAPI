9294187ba862681baeac6f273c721b9e039e1d68
Rishat Suleymanov <spk_irbis@mail.ru>
2021-05-08 11:26:19 +0300

    Refactoring

--- src/main/java/ru/lsz/App.java
+++ src/main/java/ru/lsz/App.java
@@ -19,38 +19,63 @@ import java.util.concurrent.TimeUnit;
  * Bing Wallpaper
  */
 public class App {
-    static ObjectMapper mapper = new ObjectMapper();
     public static final String URI = "https://bing.biturl.top/";
+    public static final int TIMEOUT = 1;
+    static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws InterruptedException {
+
         while (true) {
-            CloseableHttpClient httpClient = HttpClientBuilder.create()
-                    .setDefaultRequestConfig(RequestConfig.custom()
-                            .setConnectTimeout(5000)
-                            .setSocketTimeout(30000)
-                            .setRedirectsEnabled(false)
-                            .build())
-                    .build();
 
-            HttpGet request = new HttpGet(URI);
+            downloadAndSaveImage();
 
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                ApiBingWallPaper apiBingWallPaper = mapper.readValue(response.getEntity().getContent(), ApiBingWallPaper.class);
+            TimeUnit.DAYS.sleep(TIMEOUT);
 
-                saveImageToFile(apiBingWallPaper.getUrl());
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            TimeUnit.DAYS.sleep(1);
         }
+    }
+
+    private static CloseableHttpClient getCloseableHttpClient() {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        return httpClient;
 
     }
 
     private static void saveImageToFile(String url) {
+
         String[] pathParse = url.split("=");
+
         try (InputStream in = new URL(url).openStream()) {
+
             Files.copy(in, Paths.get(pathParse[pathParse.length - 1]), StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    private static void downloadAndSaveImage() {
+
+        CloseableHttpClient httpClient = getCloseableHttpClient();
+
+        HttpGet request = new HttpGet(URI);
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+            ApiBingWallPaper apiBingWallPaper = mapper.readValue(response.getEntity().getContent(), ApiBingWallPaper.class);
+
+            saveImageToFile(apiBingWallPaper.getUrl());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
